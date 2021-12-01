012421c78655b2307ba60df4c473cdcf4516a13e
Boris <borissheglov92@gmail.com>
2021-12-01 15:12:43 +0300

    onValueChenge

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -8,7 +8,7 @@ const EmployeesList = ({data}) => {
     const elements = data.map(item => {
         const {id, ...itemProps} = item; // алгоритм служит для оптимизации скорости работы приложения
         return (
-            <EmployeesListItem key={id} {...itemProps}/> // {...item}  name={item.name} salary={item.salary} increase={item.increase} получится тоже самое как с props Spreфd - оператор
+            <EmployeesListItem key={id} {...itemProps}/> // {...item},  name={item.name} salary={item.salary} increase={item.increase} получится тоже самое как с props Spreфd - оператор
         )
     })
 
